d68c0192ee05395a92d900959fcfffd664e2fcd1
mbohbless <mbohblesspearl@gmail.com>
2024-08-20 22:55:25 +0200

    correcting the sh script

--- .devcontainer/devbash.sh
+++ .devcontainer/devbash.sh
@@ -5,33 +5,34 @@
 touch .env
 
 # Set up the environment
-while [[-z "$DATABASE_URI"]]; do
+while [[ -z "$DATABASE_URI" ]]; do
     echo "Enter the database URI: "
     read DATABASE_URI
-    if [[-z "$DATABASE_URI"]]; then
+    if [[ -z "$DATABASE_URI" ]]; then
         echo "Database URI cannot be empty."
     fi
 done
-while [[-z "$TARGET_COLLECTION"]]; do
+
+while [[ -z "$TARGET_COLLECTION" ]]; do
     echo "Enter the target collection: "
     read TARGET_COLLECTION
-    if [[-z "$TARGET_COLLECTION"]]; then
+    if [[ -z "$TARGET_COLLECTION" ]]; then
         echo "Target collection cannot be empty."
     fi
 done
 
-while [[-z "$DATABASE_NAME"]]; do
+while [[ -z "$DATABASE_NAME" ]]; do
     echo "Enter the database name: "
     read DATABASE_NAME
-    if [[-z "$DATABASE_NAME"]]; then
+    if [[ -z "$DATABASE_NAME" ]]; then
         echo "Database name cannot be empty."
     fi
 done
 
-while [[-z "$EMBEDDING_SERVICE_URL"]]; do
+while [[ -z "$EMBEDDING_SERVICE_URL" ]]; do
     echo "Enter the embedding service URL: "
     read EMBEDDING_SERVICE_URL
-    if [[-z "$EMBEDDING_SERVICE_URL"]]; then
+    if [[ -z "$EMBEDDING_SERVICE_URL" ]]; then
         echo "Embedding service URL cannot be empty."
     fi
 done
@@ -47,15 +48,12 @@ echo "TARGET_COLLECTION=$TARGET_COLLECTION" >> .env
 echo "DATABASE_NAME=$DATABASE_NAME" >> .env
 echo "EMBEDDING_SERVICE_URL=$EMBEDDING_SERVICE_URL" >> .env
 
-if [[-z "$HF_TOKEN"]]; then
+if [[ -z "$HF_TOKEN" ]]; then
     echo "HF_TOKEN=$HF" >> .env
 fi
 
-if [[-z "$GROQ_API_KEY"]]; then
+if [[ -z "$GROQ_API_KEY" ]]; then
     echo "GROQ_API_KEY=$GROQ_API_KEY" >> .env
 fi
 
 echo "Environment set up successfully."
-
-
-
